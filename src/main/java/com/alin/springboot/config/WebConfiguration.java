8ca16da1752b73120a1d335c25992d295f9047f1
linying <380118294@qq.com>
2018-05-14 15:53:26 +0800

    '增加过滤器'

---
+++ src/main/java/com/alin/springboot/config/WebConfiguration.java
@@ -0,0 +1,10 @@
+package com.alin.springboot.config;
+
+/**
+ * @Description 类描述：
+ * @Author 创建人：linying
+ * @Date 创建时间：2018/5/14 15:40
+ * @Version 版本号：v1.0.0
+ */
+public class WebConfiguration {
+}
